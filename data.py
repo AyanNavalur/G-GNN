3fe07138b0660ed4a1f543cc6c034255af67cf3b
alip67 <alip67@users.noreply.github.com>
2022-08-10 19:30:43 -0400

    Commit all of the files-First commit

---
+++ data.py
@@ -0,0 +1,269 @@
+"""
+Code partially copied from 'Diffusion Improves Graph Learning' repo https://github.com/klicperajo/gdc/blob/master/data.py
+"""
+
+import os
+
+import numpy as np
+
+import torch
+from torch_geometric.data import Data, InMemoryDataset
+from torch_geometric.datasets import Planetoid, Amazon, Coauthor
+# from graph_rewiring import get_two_hop, apply_gdc
+from ogb.nodeproppred import PygNodePropPredDataset
+import torch_geometric.transforms as T
+from torch_geometric.utils import to_undirected
+from sklearn.model_selection import ShuffleSplit
+# from graph_rewiring import make_symmetric, apply_pos_dist_rewire
+from heterophilic import WebKB, WikipediaNetwork, Actor
+from torch_geometric.utils import to_scipy_sparse_matrix 
+from utils import ROOT_DIR, generate_sparse_adj
+
+DATA_PATH = f'{ROOT_DIR}/data'
+
+
+# def rewire(data, opt, data_dir):
+#   rw = opt['rewiring']
+#   if rw == 'two_hop':
+#     data = get_two_hop(data)
+#   elif rw == 'gdc':
+#     data = apply_gdc(data, opt)
+#   elif rw == 'pos_enc_knn':
+#     data = apply_pos_dist_rewire(data, opt, data_dir)
+#   return data
+
+
+def get_dataset(opt: dict, data_dir, use_lcc: bool = False) -> InMemoryDataset:
+  ds = opt['dataset']
+  path = os.path.join(data_dir, ds)
+  if ds in ['Cora', 'Citeseer', 'Pubmed']:
+    dataset = Planetoid(path, ds)
+  elif ds in ['Computers', 'Photo']:
+    dataset = Amazon(path, ds)
+  elif ds == 'CoauthorCS':
+    dataset = Coauthor(path, 'CS')
+  elif ds in ['cornell', 'texas', 'wisconsin']:
+    dataset = WebKB(root=path, name=ds, transform=T.NormalizeFeatures())
+  elif ds in ['chameleon', 'squirrel']:
+    dataset = WikipediaNetwork(root=path, name=ds, transform=T.NormalizeFeatures())
+  elif ds == 'film':
+    dataset = Actor(root=path, transform=T.NormalizeFeatures())
+  elif ds == 'ogbn-arxiv':
+    dataset = PygNodePropPredDataset(name=ds, root=path,
+                                     transform=T.ToSparseTensor())
+    use_lcc = False  # never need to calculate the lcc with ogb datasets
+  else:
+    raise Exception('Unknown dataset.')
+
+  if use_lcc:
+    lcc = get_largest_connected_component(dataset)
+
+    x_new = dataset.data.x[lcc]
+    y_new = dataset.data.y[lcc]
+
+    row, col = dataset.data.edge_index.numpy()
+    edges = [[i, j] for i, j in zip(row, col) if i in lcc and j in lcc]
+    edges = remap_edges(edges, get_node_mapper(lcc))
+
+    data = Data(
+      x=x_new,
+      edge_index=torch.LongTensor(edges),
+      y=y_new,
+      train_mask=torch.zeros(y_new.size()[0], dtype=torch.bool),
+      test_mask=torch.zeros(y_new.size()[0], dtype=torch.bool),
+      val_mask=torch.zeros(y_new.size()[0], dtype=torch.bool)
+    )
+    dataset.data = data
+  # if opt['rewiring'] is not None:
+  #   dataset.data = rewire(dataset.data, opt, data_dir)
+  train_mask_exists = True
+  try:
+    dataset.data.train_mask
+  except AttributeError:
+    train_mask_exists = False
+
+  if ds == 'ogbn-arxiv':
+    split_idx = dataset.get_idx_split()
+    ei = to_undirected(dataset.data.edge_index)
+    data = Data(
+    x=dataset.data.x,
+    edge_index=ei,
+    y=dataset.data.y,
+    train_mask=split_idx['train'],
+    test_mask=split_idx['test'],
+    val_mask=split_idx['valid'])
+    dataset.data = data
+    train_mask_exists = True
+
+
+  # Add the sparse tensor represnattion of the 
+  adj = to_scipy_sparse_matrix(dataset.data.edge_index)
+  data.adj_t = generate_sparse_adj(adj)
+  data.adj_t = data.adj_t.to_symmetric()
+  dataset.data = data
+
+  #todo this currently breaks with heterophilic datasets if you don't pass --geom_gcn_splits
+  if (use_lcc or not train_mask_exists) and not opt['geom_gcn_splits'] and opt['split_policy'] == 'fixed':
+    dataset.data = set_fixed_train_val_test_split(
+      12345,
+      dataset.data,
+      num_development=5000 if ds == "CoauthorCS" else 1500)
+  if (use_lcc or not train_mask_exists) and not opt['geom_gcn_splits'] and opt['split_policy'] == 'ratio':
+    dataset.data = set_ratio_train_valid_test_split(
+      dataset.data,opt['train_ratio'],opt['num_splits'], opt['global_random_seed'])
+
+  return dataset
+
+
+def get_component(dataset: InMemoryDataset, start: int = 0) -> set:
+  visited_nodes = set()
+  queued_nodes = set([start])
+  row, col = dataset.data.edge_index.numpy()
+  while queued_nodes:
+    current_node = queued_nodes.pop()
+    visited_nodes.update([current_node])
+    neighbors = col[np.where(row == current_node)[0]]
+    neighbors = [n for n in neighbors if n not in visited_nodes and n not in queued_nodes]
+    queued_nodes.update(neighbors)
+  return visited_nodes
+
+
+def get_largest_connected_component(dataset: InMemoryDataset) -> np.ndarray:
+  remaining_nodes = set(range(dataset.data.x.shape[0]))
+  comps = []
+  while remaining_nodes:
+    start = min(remaining_nodes)
+    comp = get_component(dataset, start)
+    comps.append(comp)
+    remaining_nodes = remaining_nodes.difference(comp)
+  return np.array(list(comps[np.argmax(list(map(len, comps)))]))
+
+
+def get_node_mapper(lcc: np.ndarray) -> dict:
+  mapper = {}
+  counter = 0
+  for node in lcc:
+    mapper[node] = counter
+    counter += 1
+  return mapper
+
+
+def remap_edges(edges: list, mapper: dict) -> list:
+  row = [e[0] for e in edges]
+  col = [e[1] for e in edges]
+  row = list(map(lambda x: mapper[x], row))
+  col = list(map(lambda x: mapper[x], col))
+  return [row, col]
+
+
+def set_fixed_train_val_test_split(
+        seed: int,
+        data: Data,
+        num_development: int = 1500,
+        num_per_class: int = 20) -> Data:
+  rnd_state = np.random.RandomState(seed)
+  num_nodes = data.y.shape[0]
+  development_idx = rnd_state.choice(num_nodes, num_development, replace=False)
+  test_idx = [i for i in np.arange(num_nodes) if i not in development_idx]
+
+  train_idx = []
+  rnd_state = np.random.RandomState(seed)
+  for c in range(data.y.max() + 1):
+    class_idx = development_idx[np.where(data.y[development_idx].cpu() == c)[0]]
+    train_idx.extend(rnd_state.choice(class_idx, num_per_class, replace=False))
+
+  val_idx = [i for i in development_idx if i not in train_idx]
+
+  def get_mask(idx):
+    mask = torch.zeros(num_nodes, dtype=torch.bool)
+    mask[idx] = 1
+    return mask
+
+  data.train_mask = get_mask(train_idx)
+  data.val_mask = get_mask(val_idx)
+  data.test_mask = get_mask(test_idx)
+
+  return data
+
+def treat_unique_class(y,dict_rep_set):
+    """
+    This function ensures the training data set has at least 1 sample per class.
+    Input:
+        y: label, could be a 0,1 matrix (number of samples x number of classes)
+    Output:
+        rep_set: representative indices of all samples (1 per class)
+        rest_set: indices not part of rep_set
+    Note: When using ShuffleSplit, only shuffle the indices not part of rep_set because
+            rep_set will always be a part of training data
+        Use the following code to find the rest of the indices that is not part of rep_set
+
+    """
+    ids= {}
+    u, ui, uc = np.unique(y, return_counts=True, return_index=True)
+    for i in range(len(dict_rep_set.keys())): 
+      rep_set = dict_rep_set[i]
+      rest_set = np.delete(range(len(y)), rep_set)
+      dict_rep = {"rep_set": rep_set, "rest_set": rest_set}
+      ids[i] = dict_rep
+    return ids
+
+def set_ratio_train_valid_test_split(data: Data, train_ratio:float = 0.32, n_splits:int = 1, random_state: int = 0) -> Data: 
+    
+    global_random_state = np.random.RandomState(random_state)
+    splits_seeds_lst = global_random_state.randint(0, 1000000, n_splits)
+    dict_Split_idx = {}
+
+    y = data.y.numpy()
+    num_nodes = data.y.shape[0]
+
+    uniq_classes = np.unique(y)
+    uniq_counts = {level: sum(y == level) for level in uniq_classes}
+    groupby_classes = {}
+    for ii, level in enumerate(uniq_classes):
+        obs_idx = [idx for idx, val in enumerate(y) if val == level]
+        groupby_classes[level] = obs_idx
+
+
+    dict_rep_set= {}    
+    for i in range(n_splits):
+      rep_set = []
+      for key,values in groupby_classes.items():
+        rnd_state = np.random.RandomState(splits_seeds_lst[i])
+        rep = rnd_state.choice(values, 1, replace=False)
+        rep_set.append(rep[0])
+      dict_rep_set[i] = rep_set
+
+
+    ids = treat_unique_class(y,dict_rep_set)
+    
+    index = 0
+    shuffle = ShuffleSplit(n_splits=n_splits, test_size=1-train_ratio,
+                           random_state=random_state)
+
+    for train_i, test_i in shuffle.split(y[ids[index]['rest_set']]):
+        list_split_idx_3 = []
+        stratSplit = ShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+        for train_idx, test_idx in stratSplit.split(ids[index]['rest_set'][train_i]):
+            X_train = ids[index]['rest_set'][train_i][train_idx]
+            X_valid = ids[index]['rest_set'][train_i][test_idx]
+            train_index = np.concatenate([ids[index]['rep_set'], X_train])
+            test_index = ids[index]['rest_set'][test_i]
+            assert len(set(train_index) & set(test_index)) == 0
+            # assert len(train_index) + len(test_index) + len(X_valid) == X.shape[0]
+            split = {"train": torch.from_numpy(train_index), "valid": (torch.from_numpy(X_valid)).type(torch.int64) , "test": (torch.from_numpy(test_index)).type(torch.int64)}
+            list_split_idx_3.append(split)
+        dict_Split_idx[index] = list_split_idx_3.copy()
+        index += 1
+
+    def get_mask(idx):
+      mask = torch.zeros(num_nodes, dtype=torch.bool)
+      mask[idx] = 1
+      return mask
+
+    data.train_mask = get_mask(train_index)
+    data.val_mask = get_mask(X_valid)
+    data.test_mask = get_mask(test_index)
+
+    return data
+
+    # return dict_Split_idx
---
+++ run_GNN.py
@@ -0,0 +1,280 @@
+import argparse
+import time
+import os
+from torch import Tensor
+
+import numpy as np
+import torch
+from torch_geometric.nn import GCNConv, ChebConv
+from torch_geometric.utils import to_scipy_sparse_matrix, index_to_mask  # noqa
+import torch.nn.functional as F
+from ogb.nodeproppred import Evaluator
+
+from gnn import GCN,SAGE
+from logger import Logger
+from data import get_dataset, set_fixed_train_val_test_split,set_ratio_train_valid_test_split
+from best_params import best_params_dict
+from utils import ROOT_DIR
+import json
+# from CGNN import CGNN, get_sym_adj
+# from CGNN import train as train_cgnn
+
+
+def get_optimizer(name, parameters, lr, weight_decay=0):
+  if name == 'sgd':
+    return torch.optim.SGD(parameters, lr=lr, weight_decay=weight_decay)
+  elif name == 'rmsprop':
+    return torch.optim.RMSprop(parameters, lr=lr, weight_decay=weight_decay)
+  elif name == 'adagrad':
+    return torch.optim.Adagrad(parameters, lr=lr, weight_decay=weight_decay)
+  elif name == 'adam':
+    return torch.optim.Adam(parameters, lr=lr)
+  elif name == 'adamax':
+    return torch.optim.Adamax(parameters, lr=lr, weight_decay=weight_decay)
+  else:
+    raise Exception("Unsupported optimizer: {}".format(name))
+
+
+def add_labels(feat, labels, idx, num_classes, device):
+  onehot = torch.zeros([feat.shape[0], num_classes]).to(device)
+  if idx.dtype == torch.bool:
+    idx = torch.where(idx)[0]  # convert mask to linear index
+  onehot[idx, labels.squeeze()[idx]] = 1
+
+  return torch.cat([feat, onehot], dim=-1)
+
+def mask_to_index(mask: Tensor) -> Tensor:
+    r"""Converts a mask to an index representation.
+
+    Args:
+        mask (Tensor): The mask.
+    """
+    return mask.nonzero(as_tuple=False).view(-1)
+
+def get_label_masks(data, mask_rate=0.5):
+  """
+  when using labels as features need to split training nodes into training and prediction
+  """
+  if data.train_mask.dtype == torch.bool:
+    idx = torch.where(data.train_mask)[0]
+  else:
+    idx = data.train_mask
+  mask = torch.rand(idx.shape) < mask_rate
+  train_label_idx = idx[mask]
+  train_pred_idx = idx[~mask]
+  return train_label_idx, train_pred_idx
+
+
+def train(model, data, train_idx, optimizer):
+    model.train()
+
+    optimizer.zero_grad()
+    out = model(data.x, data.adj_t)[train_idx]
+    loss = F.nll_loss(out, data.y.squeeze(1)[train_idx])
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+
+@torch.no_grad()
+def test(model, data, split_idx, evaluator):
+    model.eval()
+
+    out = model(data.x, data.adj_t)
+    y_pred = out.argmax(dim=-1, keepdim=True)
+
+    train_acc = evaluator.eval({
+        'y_true': data.y[split_idx['train']],
+        'y_pred': y_pred[split_idx['train']],
+    })['acc']
+    valid_acc = evaluator.eval({
+        'y_true': data.y[split_idx['valid']],
+        'y_pred': y_pred[split_idx['valid']],
+    })['acc']
+    test_acc = evaluator.eval({
+        'y_true': data.y[split_idx['test']],
+        'y_pred': y_pred[split_idx['test']],
+    })['acc']
+
+    return train_acc, valid_acc, test_acc
+
+
+
+
+def print_model_params(model):
+  print(model)
+  for name, param in model.named_parameters():
+    if param.requires_grad:
+      print(name)
+      print(param.data.shape)
+
+
+def merge_cmd_args(cmd_opt, opt):
+  if cmd_opt['beltrami']:
+    opt['beltrami'] = True
+  if cmd_opt['function'] is not None:
+    opt['function'] = cmd_opt['function']
+  if cmd_opt['block'] is not None:
+    opt['block'] = cmd_opt['block']
+  if cmd_opt['attention_type'] != 'scaled_dot':
+    opt['attention_type'] = cmd_opt['attention_type']
+  if cmd_opt['self_loop_weight'] is not None:
+    opt['self_loop_weight'] = cmd_opt['self_loop_weight']
+  if cmd_opt['method'] is not None:
+    opt['method'] = cmd_opt['method']
+  if cmd_opt['step_size'] != 1:
+    opt['step_size'] = cmd_opt['step_size']
+  if cmd_opt['time'] != 1:
+    opt['time'] = cmd_opt['time']
+  if cmd_opt['epoch'] != 100:
+    opt['epoch'] = cmd_opt['epoch']
+  if not cmd_opt['not_lcc']:
+    opt['not_lcc'] = False
+  if cmd_opt['num_splits'] != 1:
+    opt['num_splits'] = cmd_opt['num_splits']
+
+
+def main(cmd_opt):
+  # try:
+  #   # best_opt = best_params_dict[cmd_opt['dataset']]
+  #   # opt = {**cmd_opt, **best_opt}
+  #   merge_cmd_args(cmd_opt, opt)
+  # except KeyError:
+  #   opt = cmd_opt
+  opt = cmd_opt
+
+  dataset = get_dataset(opt, f'{ROOT_DIR}/data', opt['not_lcc'])
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+  data = dataset[0]
+
+  if args.use_sage:
+        model = SAGE(data.num_features, opt['hidden_channels'],
+                     dataset.num_classes,opt['num_layers'],
+              opt['dropout']).to(device)
+  else:
+        model = GCN(data.num_features, opt['hidden_channels'],
+              dataset.num_classes, opt['num_layers'],
+              opt['dropout']).to(device)
+
+  if not opt['planetoid_split'] and opt['dataset'] in ['Cora','Citeseer','Pubmed']:
+    dataset.data = set_fixed_train_val_test_split(np.random.randint(0, 1000), dataset.data, num_development=5000 if opt["dataset"] == "CoauthorCS" else 1500)
+
+  data = dataset.data.to(device)
+  split_idx = {"train": mask_to_index(data.train_mask),"valid": mask_to_index(data.val_mask ), "test": mask_to_index(data.test_mask)}
+  train_idx = mask_to_index(data.train_mask).to(device)
+
+
+  evaluator = Evaluator(name='ogbn-arxiv')
+  logger = Logger(opt['num_splits'] * opt['runs'], args)
+  # internal_logger = Logger(10, args)
+  logger_report = Logger(opt['num_splits'], args)
+  logger_test = Logger(opt['num_splits'] * opt['runs'], args)
+
+  parameters = [p for p in model.parameters() if p.requires_grad]
+  print(sum(p.numel() for p in model.parameters()))
+  print_model_params(model)
+  optimizer = get_optimizer(opt['optimizer'], parameters, lr=opt['lr'], weight_decay=opt['decay'])
+  best_time = best_epoch = train_acc = val_acc = test_acc = 0
+
+  for run in range(args.runs):
+    model.reset_parameters()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    for epoch in range(1, 1 + args.epochs):
+        loss = train(model, data, train_idx, optimizer)
+        result = test(model, data, split_idx, evaluator)
+        logger.add_result(run, result)
+
+        if epoch % args.log_steps == 0:
+            train_acc, valid_acc, test_acc = result
+            print(f'Run: {run + 1:02d}, '
+                  f'Epoch: {epoch:02d}, '
+                  f'Loss: {loss:.4f}, '
+                  f'Train: {100 * train_acc:.2f}%, '
+                  f'Valid: {100 * valid_acc:.2f}% '
+                  f'Test: {100 * test_acc:.2f}%')
+    logger.print_statistics(run)
+  train_final, valid_final, test_final = logger.print_statistics()
+
+  result = {"Dataset": args.external_dataset, "Model": args.booster,"train_ratio": int(args.train_ratio),
+            "Average Train": train_final, " Average Valid": valid_final, " Average Test": test_final}
+  result_file = open(args.outputpath, "a+", encoding='utf-8')
+  result_file.write(json.dumps(result) + '\n')
+  result_file.close()
+
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--use_cora_defaults', action='store_true',
+                      help='Whether to run with best params for cora. Overrides the choice of dataset')
+  # data args
+  parser.add_argument('--dataset', type=str, default='Cora',
+                      help='Cora, Citeseer, Pubmed, Computers, Photo, CoauthorCS, ogbn-arxiv')
+  parser.add_argument('--data_norm', type=str, default='rw',
+                      help='rw for random walk, gcn for symmetric gcn norm')
+  parser.add_argument('--self_loop_weight', type=float, default=1.0, help='Weight of self-loops.')
+  parser.add_argument('--use_labels', dest='use_labels', action='store_true', help='Also diffuse labels')
+  parser.add_argument('--geom_gcn_splits', dest='geom_gcn_splits', action='store_true',
+                      help='use the 10 fixed splits from '
+                           'https://arxiv.org/abs/2002.05287')
+  parser.add_argument('--num_splits', type=int, dest='num_splits', default=1,
+                      help='the number of splits to repeat the results on')
+  parser.add_argument('--label_rate', type=float, default=0.5,
+                      help='% of training labels to use when --use_labels is set.')
+  parser.add_argument('--planetoid_split', action='store_true',
+                      help='use planetoid splits for Cora/Citeseer/Pubmed')
+  parser.add_argument('--not_lcc', action='store_false',
+                      help='use largest coonected component')
+  parser.add_argument("--global_random_seed", type=int, default=2021,
+                        help="Random seed (for reproducibility).")
+  parser.add_argument("--outputpath", type=str, default="empty.json",
+                      help="outputh file path to save the result")
+  parser.add_argument("--train_ratio", type=float, default=1.,
+                      help="the start value of the train ratio (inclusive).")
+  parser.add_argument("--split_policy", type=str, default='fixed',
+                      help="")
+  # GNN args
+  parser.add_argument('--device', type=int, default=0)
+  parser.add_argument('--log_steps', type=int, default=1)
+  parser.add_argument('--use_sage', action='store_true')
+  parser.add_argument('--num_layers', type=int, default=3)
+  parser.add_argument('--hidden_channels', type=int, default=256)
+  parser.add_argument('--dropout', type=float, default=0.5)
+  parser.add_argument('--lr', type=float, default=0.01)
+  parser.add_argument('--epochs', type=int, default=500)
+  parser.add_argument('--runs', type=int, default=10)
+  # parser.add_argument("--splits", type=int, default=5,
+  #                     help="The number of re-shuffling & splitting for each train ratio.")
+
+
+  # parser.add_argument('--hidden_dim', type=int, default=16, help='Hidden dimension.')
+  # parser.add_argument('--fc_out', dest='fc_out', action='store_true',
+  #                     help='Add a fully connected layer to the decoder.')
+  # parser.add_argument('--input_dropout', type=float, default=0.5, help='Input dropout rate.')
+  # parser.add_argument('--dropout', type=float, default=0.0, help='Dropout rate.')
+  # parser.add_argument("--batch_norm", dest='batch_norm', action='store_true', help='search over reg params')
+  parser.add_argument('--optimizer', type=str, default='adam', help='One from sgd, rmsprop, adam, adagrad, adamax.')
+  # parser.add_argument('--lr', type=float, default=0.01, help='Learning rate.')
+  parser.add_argument('--decay', type=float, default=5e-4, help='Weight decay for optimization')
+  # parser.add_argument('--epoch', type=int, default=100, help='Number of training epochs per iteration.')
+  # parser.add_argument('--alpha', type=float, default=1.0, help='Factor in front matrix A.')
+  # parser.add_argument('--alpha_dim', type=str, default='sc', help='choose either scalar (sc) or vector (vc) alpha')
+  # parser.add_argument('--no_alpha_sigmoid', dest='no_alpha_sigmoid', action='store_true',
+  #                     help='apply sigmoid before multiplying by alpha')
+  # parser.add_argument('--beta_dim', type=str, default='sc', help='choose either scalar (sc) or vector (vc) beta')
+  # parser.add_argument('--block', type=str, default='constant', help='constant, mixed, attention, hard_attention')
+  # parser.add_argument('--function', type=str, default='laplacian', help='laplacian, transformer, dorsey, GAT')
+  # parser.add_argument('--use_mlp', dest='use_mlp', action='store_true',
+  #                     help='Add a fully connected layer to the encoder.')
+  # parser.add_argument('--add_source', dest='add_source', action='store_true',
+  #                     help='If try get rid of alpha param and the beta*x0 source term')
+  # parser.add_argument('--cgnn', dest='cgnn', action='store_true', help='Run the baseline CGNN model from ICML20')
+
+
+  args = parser.parse_args()
+
+  opt = vars(args)
+
+  main(opt)
